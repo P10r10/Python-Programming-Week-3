9cb1bb66544eec390735140ee4fe9809af50563d
P10r10 <asantiago@keemail.me>
2023-03-16 13:14:23 +0000

    second_and_second_to_last_characters

---
+++ second_and_second_to_last_characters.py
@@ -0,0 +1,5 @@
+s = input("Please type in a string: ")
+if s[1] == s[-2]:
+    print(f"The second and the second to last characters are {s[1]}")
+else:
+    print("The second and the second to last characters are different")
