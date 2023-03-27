365f26d0e698a1e5e0caa3cb3b6587c3c217aecf
P10r10 <asantiago@keemail.me>
2023-03-27 12:12:17 +0100

    print_many_times

--- mean.py
+++ mean.py
@@ -1,7 +1,6 @@
 def mean(x, y, z):
     print((x + y + z) / 3)
 
-# Write your code here
-# Testing the function
+
 if __name__ == "__main__":
     mean(1, 2, 3)
---
+++ print_many_times.py
@@ -0,0 +1,7 @@
+def print_many_times(s, n):
+    for _ in range(n):
+        print(s)
+
+
+if __name__ == "__main__":
+    print_many_times("python", 4)
