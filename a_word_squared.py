59a5b8fdbb92740519d503e3b47f11d8d3b76da7
P10r10 <asantiago@keemail.me>
2023-03-28 12:02:33 +0100

    a_word_squared

--- a_word_squared.py
+++ a_word_squared.py
@@ -1,2 +1,13 @@
-# TODO
+def squared(s, side):
+    idx = 0
+    for _ in range(side):
+        for _ in range(side):
+            print(s[idx % len(s)], end="")
+            idx += 1
+        print()
 
+
+if __name__ == "__main__":
+    squared("ab", 3)
+    print()
+    squared("aybabtu", 5)
